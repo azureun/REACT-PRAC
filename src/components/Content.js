9eff98576b1aae85b4f9606acb6ed16771b64c2c
azureun <euny1387@gmail.com>
2024-11-22 00:11:52 +0900

    리액트 화면에 글자 컴포넌트 안 뜨는 오류 수정

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,11 +3,12 @@ import React, {Component} from "react"
 
 class Content extends Component {
     render() {
+        console.log('Content render');
         return (
             <article>
                 <h2>HTML</h2>
                 HTML is HyperText Markup Language.
-                {this.props}
+                {this.props.desc}
             </article>
         );
     }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,12 +3,17 @@ import React, {Component} from "react"
 //TOC(Table of Content) : 목차
 class TOC extends Component {
     render() {
+        console.log('TOC render')
         var lists = [];
         var data = this.props.data;
         var i = 0;
         while(i < data.length){
             /* TOC 컴포넌트는 내부적으로 this.props.data 가짐. > 이 값을 가지고 글 목록을 생성함. */
-            lists.push(<li key={data[i].id}><a href={"/content" + data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+                <li key={data[i].id}>
+                    <a href={"/content/" + data[i].id}>{data[i].title}</a>
+                </li>
+            );
             i = i + 1;
         }
         return (
@@ -19,7 +24,7 @@ class TOC extends Component {
                     <li><a href="3.HTML">JavaScript</a></li>               
                 </ul>
             </nav>
-        )
+        );
     }
 }
 
