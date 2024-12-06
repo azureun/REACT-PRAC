fb647a708eceea3ca9968185a424b11c6ccee340
azureun <euny1387@gmail.com>
2024-12-06 15:51:23 +0900

    24.12.06 / 1-19

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 import TOC from './components/TOC';
 import Content from './components/Content';
 import Subject from "./components/Subject";
+import Control from "./components/Control";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+
 import './App.css';
-
-
-//class Subject을 src/components/Subject.js안에 넣어 관리
-
-//TOC(Table of Content) : 목차
-//여기 있던 class TOC를 src/components/TOC.js안에 넣어 관리
-
-// Content 컴포넌트는 <article> 부분을 컴포넌트로 변환 한 것
-//class Content를 src/components/Content.js안에 넣어 관리
 
 class App extends Component {       
     constructor(props){         
         super(props);
+        this.max_content_id = 3;
         this.state = {  //가장 먼저 컴포넌트 초기화
             mode:'read',
             selected_content_id: 2,     //contents 리스트의 2번째 내용이 선택돼 있게 함.
@@ -30,10 +26,11 @@ class App extends Component {
     }
     render() {
         console.log('App render');
-        var _title, _desc = null;
+        var _title, _desc = null, _article = null;
         if(this.state.mode === 'welcome'){
             _title = this.state.welcome.title;
             _desc = this.state.welcome.desc;
+            _article = <ReadContent title={_title} desc={_desc}></ReadContent>
         } else if (this.state.mode === 'read') {
             var i =0;
             while(i < this.state.contents.length){
@@ -46,42 +43,55 @@ class App extends Component {
                 i = i + 1;
             }
         }
+        else if(this.state.mode === 'create'){
+            _article=<CreateContent onSubmit={function(_title, _desc){
+                this.max_content_id = this.max_content_id + 1;
+
+                //this.state.contents.push({});     //원본을 변경한다.
+                //this.state.contents.concat({});   //원본을 변경하지 않는다.
+
+                //this.state.contents.push({id:this.max_content_id, title:_title, desc:_desc});
+                //var _contents = this.state.contents.concat({id:this.max_content_id, title:this._title, desc:_desc});
+                var newContents = Array.from(this.state.contents);
+                newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+                this.setState({
+                    contents:newContents
+                });
+                console.log(_title, _desc);
+            }.bind(this)}></CreateContent>
+        }
         console.log("render", this);
 
         return (
             <div className="App" style={{textAlign: 'left'}}>
                 {
-                <Subject 
+                    <Subject 
                     title = { this.state.subject.title }
                     sub = { this.state.subject.sub }
                     onChangePage = {function() {
                         this.setState({mode:'welcome'})
-                 }.bind(this)}
-                ></Subject> 
+                    }.bind(this)}
+                ></Subject>
                 }
-                {/*<header>
-                    <h1><a href="/" onClick={function(e){
-                        console.log(e);
-                        e.preventDefault();
-                        this.setState({mode: "welcome"});   
-                    }.bind(this)}>{this.state.subject.title}</a></h1>
-                    {this.state.subject.sub}
-                </header> */}
-            
-                {/*state값을 변경하려면 
-                1. 함수 뒤에 .bind(this) 추가, 
-                2. this.setState 함수 호출해서 state 값 변경*/}
 
                 <TOC
                 onChangePage = {function (id){  //실제로 클릭한 값이 n인지 확인
                     debugger;
-                    //alert("hi");
                     this.setState({
                         mode:"read",
                         selected_content_id:Number(id)
                     });
                 }.bind(this)}                
                 data={this.state.contents}></TOC>
+
+                <Control onChangeMode={function (_mode) {
+                    this.setState({
+                        mode:_mode
+                    });
+                }.bind(this)}></Control>
+
+                {_article}
+                <ReadContent title={_title} desc={_desc}></ReadContent>
                 <Content title={_title} desc={_desc}></Content>
             </div>
         );
